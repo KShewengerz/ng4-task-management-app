6ac8a20835d4136fb4286dfd3cfb1908e5692cd1
Kristy Mae Almuete <kristyalmuete@gmail.com>
2017-11-30 20:56:20 +0800

    #6 Setup Login and Signup form UI and change mdl theme to grey-red. Setup Landing Component with Form Builders

--- public/app/src/app/landing/landing.component.ts
+++ public/app/src/app/landing/landing.component.ts
@@ -1,5 +1,9 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Credential } from './landing.model';
 
 
 @Component({
@@ -8,8 +12,44 @@ import { Component } from '@angular/core';
   templateUrl: 'landing.component.html',
   styleUrls: ['landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
-  constructor() {}
+  loginForm: FormGroup;
+  signupForm: FormGroup;
+
+  isLogin: boolean = true;
+  isSignUp: boolean = false;
+
+  firstName    = new FormControl('', Validators.required);
+  lastName     = new FormControl('', Validators.required);
+  emailAddress = new FormControl('', Validators.required);
+  username     = new FormControl('', Validators.required);
+  password     = new FormControl('', Validators.required);
+
+  constructor(private router: Router,
+              private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.loginForm = this.fb.group({
+      'username':   this.username,
+      'password':   this.password
+    });
+
+    this.signupForm = this.fb.group({
+      'firstName'   : this.firstName,
+      'lastName'    : this.lastName,
+      'emailAddress': this.emailAddress,
+      'username'    : this.username,
+      'password'    : this.password
+    });
+  }
+
+  login(user: Credential) {
+    console.log(user);
+  }
 
 }
--- public/app/src/app/landing/landing.module.ts
+++ public/app/src/app/landing/landing.module.ts
@@ -1,5 +1,8 @@
 
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MdlModule } from '@angular-mdl/core';
 
@@ -10,6 +13,10 @@ import { LandingRouting } from './landing-routing.module';
 
 @NgModule({
   imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MdlModule,
     LandingRouting
   ],
