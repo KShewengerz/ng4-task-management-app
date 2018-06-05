3b2b3ad1dbfed801d6aecc4577941424a74f058e
Kristy Mae Almuete <kristyalmuete@gmail.com>
2018-06-05 19:59:55 +0800

    Remove dist folder

--- dist/api/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-
-function main(req, res, next) {
-    res.send("Welcome to Express");
-}
-
-exports.main = main;
-//# sourceMappingURL=main.js.map
--- dist/public/app/src/app/landing/landing-routing.module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const router_1 = require("@angular/router");
-const landing_component_1 = require("./landing.component");
-const routes = [
-    { path: "", component: landing_component_1.LandingComponent }
-];
-exports.LandingRouting = router_1.RouterModule.forChild(routes);
-//# sourceMappingURL=landing-routing.module.js.map
