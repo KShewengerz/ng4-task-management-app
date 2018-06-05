576c5bc4b8d7e7a05954982dc49c740375a54ecf
Kristy Mae Almuete <kristyalmuete@gmail.com>
2018-06-05 21:32:10 +0800

    #33 Add Auth Guard Service on Landing and Dashboard Routes.

--- public/app/src/app/app-routing.module.ts
+++ public/app/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { RouterModule, Routes } from "@angular/router";
 
+import { AuthGuardService } from "./auth/auth-guard.service";
 
 const routes: Routes = [
-  { path: "", loadChildren: "./landing/landing.module#LandingModule" },
-  { path: "dashboard", loadChildren: "./dashboard/dashboard.module#DashboardModule" }
+  { path: "", loadChildren: "./landing/landing.module#LandingModule", canActivate: [AuthGuardService] },
+  { path: "dashboard", loadChildren: "./dashboard/dashboard.module#DashboardModule", canActivate: [AuthGuardService] }
 ];
 
 export const AppRouting = RouterModule.forRoot(routes, { useHash: false });
--- public/app/src/app/app.module.ts
+++ public/app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 
+import { AuthGuardService } from "./auth/auth-guard.service";
+
 import { AppRouting } from "./app-routing.module";
 
 
@@ -12,7 +14,10 @@ import { AppRouting } from "./app-routing.module";
     AppRouting
   ],
   declarations: [ AppComponent ],
-  providers: [{ provide: "API_URL", useValue: "http://localhost:3000" }],
+  providers: [
+    AuthGuardService,
+    { provide: "API_URL", useValue: "http://localhost:3000" }
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
